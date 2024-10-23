afda414e9650b209a96829ecafd25863e71ea167
Minner22 <p_makuch@poczta.onet.pl>
2024-10-23 23:18:27 +0200

    Update AbstractEntity.java
    changed generation type to SEQUENCE for postgresql optimalization

--- src/main/java/pl/m22/gamehive/common/AbstractEntity.java
+++ src/main/java/pl/m22/gamehive/common/AbstractEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public abstract class AbstractEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @Column(updatable = false)
     private LocalDateTime createdAt;
