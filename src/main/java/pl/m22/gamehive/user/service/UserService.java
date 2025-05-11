36171577b369e7a1ca7e85b8bb178281c406ab8e
Minner22 <p_makuch@poczta.onet.pl>
2025-05-11 21:02:24 +0200

    Add interface for user service to separate contract and implementation

--- src/main/java/pl/m22/gamehive/user/service/UserService.java
+++ src/main/java/pl/m22/gamehive/user/service/UserService.java
@@ -1,68 +1,16 @@
 package pl.m22.gamehive.user.service;
 
-import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import pl.m22.gamehive.user.dto.UserCredentialsDto;
 import pl.m22.gamehive.user.dto.UserRegistrationDto;
-import pl.m22.gamehive.user.exception.RoleNotFoundException;
-import pl.m22.gamehive.user.mapper.UserMapper;
-import pl.m22.gamehive.user.model.AppUser;
-import pl.m22.gamehive.user.model.UserDetails;
-import pl.m22.gamehive.user.model.UserRole;
-import pl.m22.gamehive.user.repository.UserRepository;
-import pl.m22.gamehive.user.repository.UserRoleRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-@AllArgsConstructor
-public class UserService {
-
-    private static final String USER_ROLE = "USER";
-    private final UserRepository userRepository;
-    private final UserRoleRepository userRoleRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public Optional<UserCredentialsDto> findCredentialsByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .map(UserMapper.INSTANCE::toUserCredentialsDto);
-    }
-
-    public List<String> findAllUserEmails() {
-        return userRepository.findAllUsersByRoles_Name(USER_ROLE).stream()
-                .map(AppUser::getEmail)
-                .toList();
-    }
-
-    @Transactional
-    public void deleteUserByEmail(String email) {
-        userRepository.deleteByEmail(email);
-    }
-
-    @Transactional
-    public void register(UserRegistrationDto registrationDto) {
-        AppUser appUser = UserMapper.INSTANCE.toUser(registrationDto);
-        Optional<UserRole> userRole = userRoleRepository.findByName(USER_ROLE);
-        String passwordHash = passwordEncoder.encode(registrationDto.password());
-        appUser.setPassword(passwordHash);
-        userRole.ifPresentOrElse(
-                role -> appUser.getRoles().add(role),
-                () -> {
-                    throw new RoleNotFoundException(USER_ROLE);
-                }
-        );
-        appUser.setUserDetails(new UserDetails());
-        userRepository.save(appUser);
-    }
-
-    public boolean emailExists(String email) {
-        return userRepository.findByEmail(email).isPresent();
-    }
-
-    public boolean usernameExists(String username) {
-        return userRepository.findByUsername(username).isPresent();
-    }
+public interface UserService {
+    Optional<UserCredentialsDto> findCredentialsByEmail(String email);
+    List<String> findAllUserEmails();
+    void deleteUserByEmail(String email);
+    void register(UserRegistrationDto registrationDto);
+    boolean emailExists(String email);
+    boolean usernameExists(String username);
 }
