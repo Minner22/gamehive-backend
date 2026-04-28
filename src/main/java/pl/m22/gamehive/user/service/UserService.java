dbacfb6280f9980569c86b0d5874dbd0fbe0d2b4
Minner22 <p_makuch@poczta.onet.pl>
2026-04-28 22:47:32 +0200

    updated user service and tests

--- src/main/java/pl/m22/gamehive/user/service/UserService.java
+++ src/main/java/pl/m22/gamehive/user/service/UserService.java
@@ -1,7 +1,11 @@
 package pl.m22.gamehive.user.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import pl.m22.gamehive.auth.dto.CredentialsDto;
+import pl.m22.gamehive.user.dto.UserProfileUpdateDto;
 import pl.m22.gamehive.user.model.AppUser;
+import pl.m22.gamehive.user.model.UserProfile;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +19,12 @@ public interface UserService {
     boolean usernameExists(String username);
     List<AppUser> findAllUsers();
     Optional<AppUser> findUserByEmail(String email);
+
+    AppUser findUserById(Long id);
+    AppUser findUserByUsername(String username);
+
+    Page<AppUser> findAllUsers(Pageable pageable);
+
+    UserProfile updateCurrentUserProfile(String email, UserProfileUpdateDto userProfileUpdateDto);
 
 }
--- src/main/java/pl/m22/gamehive/user/service/UserServiceImpl.java
+++ src/main/java/pl/m22/gamehive/user/service/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package pl.m22.gamehive.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.m22.gamehive.auth.dto.CredentialsDto;
+import pl.m22.gamehive.common.exception.DomainException;
+import pl.m22.gamehive.common.exception.ErrorCode;
+import pl.m22.gamehive.user.dto.UserProfileUpdateDto;
 import pl.m22.gamehive.user.mapper.UserMapper;
 import pl.m22.gamehive.user.model.AppUser;
+import pl.m22.gamehive.user.model.UserProfile;
 import pl.m22.gamehive.user.repository.UserRepository;
-import pl.m22.gamehive.user.repository.UserRoleRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +23,6 @@ public class UserServiceImpl implements UserService {
 
     private static final String USER_ROLE = "ROLE_USER";
     private final UserRepository userRepository;
-    private final UserRoleRepository userRoleRepository;
     private final UserMapper userMapper;
 
     @Override
@@ -58,5 +62,39 @@ public class UserServiceImpl implements UserService {
     @Override
     public Optional<AppUser> findUserByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public AppUser findUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new DomainException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    @Override
+    public AppUser findUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new DomainException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    @Override
+    public Page<AppUser> findAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @Override
+    public UserProfile updateCurrentUserProfile(String email, UserProfileUpdateDto userProfileUpdateDto) {
+        AppUser user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new DomainException(ErrorCode.USER_NOT_FOUND));
+        UserProfile profile = user.getUserProfile();
+
+        if (profile == null) {
+            profile = new UserProfile();
+            user.setUserProfile(profile);
+        }
+
+        userMapper.updateUserProfileFromDto(userProfileUpdateDto, profile);
+        userRepository.save(user);
+
+        return profile;
     }
 }
