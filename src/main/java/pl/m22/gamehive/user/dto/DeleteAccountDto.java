63c0dd9c00f8520e6a9b178e5b34f6a65dab95f9
Minner22 <p_makuch@poczta.onet.pl>
2026-06-22 20:59:01 +0200

    GH-108 dodanie DeleteAccountDto dla samodzielnego usunięcia konta

---
+++ src/main/java/pl/m22/gamehive/user/dto/DeleteAccountDto.java
@@ -0,0 +1,14 @@
+package pl.m22.gamehive.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+@Schema(description = "Potwierdzenie usunięcia konta hasłem.")
+public record DeleteAccountDto(
+
+        @Schema(description = "Hasło użytkownika, wymagane do potwierdzenia usunięcia konta.",
+                example = "P@ssw0rd123", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank(message = "Password must not be blank")
+        String password
+) {
+}
