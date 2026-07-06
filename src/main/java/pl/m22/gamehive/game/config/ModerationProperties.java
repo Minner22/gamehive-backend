37643dd02c54a564b475388565b93b7bb9b094cf
Minner22 <p_makuch@poczta.onet.pl>
2026-07-06 21:55:09 +0200

    GH-117 konfiguracja max-resubmissions + nowe ErrorCode

---
+++ src/main/java/pl/m22/gamehive/game/config/ModerationProperties.java
@@ -0,0 +1,14 @@
+package pl.m22.gamehive.game.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "gamehive.moderation")
+public class ModerationProperties {
+    private int maxResubmissions;
+}
